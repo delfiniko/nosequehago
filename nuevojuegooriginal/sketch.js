623b6b4c0448a52c86e406f24559d61c31513c9f
delfiniko <107224412+delfiniko@users.noreply.github.com>
2022-06-09 17:57:25 -0500

    Add files via upload

---
+++ nuevojuegooriginal/sketch.js
@@ -0,0 +1,20 @@
+console.log("#savetf2")
+var trex ,trex_running;
+var esamNUm1
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(700,200)
+  esamNUm1 = createSprite(100, 100, 10, 10)
+  //create a trex sprite
+ 
+}
+
+function draw(){
+  background("blue")
+  drawSprites();
+
+}
